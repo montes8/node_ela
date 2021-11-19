ec4856461a001c818478a266d69641f8a9d55dd1
montes8 <emontesinos812@gmail.com>
2021-11-19 16:45:26 -0500

    actualizando nombre de ruta de listado de usuarios

--- models/server.js
+++ models/server.js
@@ -8,7 +8,7 @@ class Server {
     constructor() {
         this.app  = express();
         this.port = process.env.PORT;
-        this.usuariosPath = '/api/user';
+        this.usuariosPath = '/api/users';
 
         // Conectar a base de datos
         this.conectarDB();
