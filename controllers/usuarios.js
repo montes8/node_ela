95719ec574d5f2aa0c2cf3e7785f1003bc4da7f0
montes8 <emontesinos812@gmail.com>
2021-11-19 19:34:38 -0500

    update heroku

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -12,15 +12,15 @@ const usuariosGet = async(req = request, res = response) => {
     const { limite = 5, desde = 0 } = req.query;
     const query = { estado: true };
 
-    const [ total, usuarios ] = await Promise.all([
-        Usuario.countDocuments(query),
+    const [usuarios ] = await Promise.all([
+        //Usuario.countDocuments(query),
         Usuario.find(query)
             .skip( Number( desde ) )
             .limit(Number( limite ))
     ]).catch(error => { throw error});
 
     res.json({
-        total,
+       // total,
         usuarios
     });
 
